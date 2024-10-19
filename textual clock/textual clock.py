9572aa291ffaf66cd3887de7e8d10df44e632825
RandomMonkey99 <abel.bochsler@proton.me>
2024-10-19 15:05:10 +0200

    Update textual clock.py

--- textual clock/textual clock.py
+++ textual clock/textual clock.py
@@ -6,7 +6,7 @@ from termcolor import colored #before you have to install termcolor : pip instal
 
 def time():
     string=strftime('%A %D %H:%M:%S: %p')
-    print(colored(string, "green")#you can also replace with another color
+    print(colored(string, "green"))#you can also replace with another color
     sleep(1.00)
     os.system("cls")
     time()
